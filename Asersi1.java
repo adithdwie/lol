4b3451b4d98d941c9fa6acbab20585ddba4331cf
Adith Dwiesha <patriaadith@gmail.com>
2016-09-22 21:35:42 +0700

    ada

--- Asersi1.java
+++ Asersi1.java
@@ -4,7 +4,7 @@ public class Asersi1{
 		if(x>0){
 			System.out.println("x bilangan positif");
 		}else{
-			assert(x<0):"Afa kesalahan code";
+			assert(x<0):"Ada kesalahan code";
 			System.out.println("x bilangan negatif");
 		}
 	}
